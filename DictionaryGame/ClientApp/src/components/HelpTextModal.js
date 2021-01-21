d6e0b0a4993dea19cd76966dedc4cbbd5b6c68da
Aaron Campbell <aaron.campbell.ncfca@gmail.com>
2021-01-21 08:48:31 -0500

    Update HelpTextModal.js

--- DictionaryGame/ClientApp/src/components/HelpTextModal.js
+++ DictionaryGame/ClientApp/src/components/HelpTextModal.js
@@ -5,13 +5,13 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 export function HelpTextModal({ modalOpen, setModalOpen }) {
     
     return (
-        <Modal isOpen={modalOpen} toggle={() => setModalOpen(!modalOpen)}>
+        <Modal isOpen={modalOpen} toggle={() => setModalOpen(!modalOpen)} size="lg">
             <ModalHeader>How to Play</ModalHeader>
             <ModalBody>
                 <HelpText />
             </ModalBody>
             <ModalFooter>
-                <Button color="primary" onClick={() => setModalOpen(false)}>Cancel</Button>
+                <Button color="primary" onClick={() => setModalOpen(false)}>Close</Button>
             </ModalFooter>
         </Modal>
     );
